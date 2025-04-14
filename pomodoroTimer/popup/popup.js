a1bff0c9f4e4803d21e20e1d90cc7157a9e6cc5a
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-04-14 20:57:46 +0300

    Add task feature by storage

--- pomodoroTimer/popup/popup.js
+++ pomodoroTimer/popup/popup.js
@@ -1,7 +1,16 @@
-const tasks = [];
+let tasks = [];
 
 const addTaskButton = document.getElementById("add-task-button");
 addTaskButton.addEventListener("click", () => addTask());
+chrome.storage.sync.get(["tasks"], (res) => {
+  tasks = res.tasks ? res.tasks : [];
+  renderTasks();
+});
+function saveTasks() {
+  chrome.storage.sync.set({
+    tasks,
+  });
+}
 
 function renderTask(taskNum) {
   const taskRow = document.createElement("div");
@@ -12,6 +21,7 @@ function renderTask(taskNum) {
 
   text.addEventListener("change", () => {
     tasks[taskNum] = text.value;
+    saveTasks();
   });
   const deleteButton = document.createElement("input");
   deleteButton.type = "button";
@@ -30,10 +40,12 @@ function addTask() {
   const taskNum = tasks.length;
   tasks.push("");
   renderTask(taskNum);
+  saveTasks();
 }
 function deleteTask(taskNum) {
   tasks.splice(taskNum, 1);
   renderTasks();
+  saveTasks();
 }
 function renderTasks() {
   const taskContainer = document.getElementById("task-container");
