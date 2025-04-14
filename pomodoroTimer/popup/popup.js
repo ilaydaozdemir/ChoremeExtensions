5f83a5e61b137bd585826ce36d5caf49f5468a68
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-04-14 20:21:07 +0300

    Add delete task feature

--- pomodoroTimer/popup/popup.js
+++ pomodoroTimer/popup/popup.js
@@ -3,13 +3,12 @@ const tasks = [];
 const addTaskButton = document.getElementById("add-task-button");
 addTaskButton.addEventListener("click", () => addTask());
 
-function addTask() {
-  const taskNum = tasks.length;
-  tasks.push("");
+function renderTask(taskNum) {
   const taskRow = document.createElement("div");
   const text = document.createElement("input");
   text.type = "text";
   text.placeholder = "Enter a task ...";
+  text.value = tasks[taskNum];
 
   text.addEventListener("change", () => {
     tasks[taskNum] = text.value;
@@ -18,12 +17,28 @@ function addTask() {
   deleteButton.type = "button";
   deleteButton.value = "X";
   deleteButton.addEventListener("click", () => {
-    tasks.splice(taskNum, 1);
+    deleteTask(taskNum);
   });
-
   taskRow.appendChild(text);
   taskRow.appendChild(deleteButton);
 
   const taskContainer = document.getElementById("task-container");
   taskContainer.appendChild(taskRow);
+}
+
+function addTask() {
+  const taskNum = tasks.length;
+  tasks.push("");
+  renderTask(taskNum);
+}
+function deleteTask(taskNum) {
+  tasks.splice(taskNum, 1);
+  renderTasks();
+}
+function renderTasks() {
+  const taskContainer = document.getElementById("task-container");
+  taskContainer.textContent = "";
+  tasks.forEach((taskText, taskNum) => {
+    renderTask(taskNum);
+  });
 }
