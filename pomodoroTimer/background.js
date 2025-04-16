5e0e3a686e7e51455bbd419d50a1dce51df89e0f
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-04-16 18:13:12 +0300

    Add notification for 25 minutes passed

--- pomodoroTimer/background.js
+++ pomodoroTimer/background.js
@@ -6,8 +6,18 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.local.get(["timer", "isRunning"], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1;
+        let isRunning = true;
+        if (timer === 60 * 25) {
+          this.registration.showNotification("Pomodoro Timer", {
+            body: "25 minutes has passed!",
+            icon: "icon.png",
+          });
+          timer = 0;
+          isRunning: false;
+        }
         chrome.storage.local.set({
           timer,
+          isRunning,
         });
       }
     });
